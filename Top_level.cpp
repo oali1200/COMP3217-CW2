885c51036c5fca74abcaab9dab94ef3671ffb19a
oali1200 <83743492+oali1200@users.noreply.github.com>
2021-05-06 05:09:38 +0100

    Created the main function Put in comments that describe how function should work

---
+++ Top_level.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+//Declaring a function that generates the Linear Programs
+//Declaring a function that saves the values from the output text file
+
+
+int main()
+{
+    //We want to open our files here
+    //We want to read the excel file here for user task allocations
+    //Create a for loop that iterates for each of the 10,000 pricing curves
+
+    //Call a function that Generates the Linear Program while feeding into it the iteration we are in so that the function can use the correct pricing guideline
+    //Use a system call that calls on lpsolve to run the LP
+    //Call a function that saves the values from the output text file made by LP solve into seperate text files
+
+    //We want to close our files here
+    return 0;
+}
+
+//Create a function that generates the Linear Programs
+
+//Create a function that saves the values from the output text file
