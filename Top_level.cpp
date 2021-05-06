81ae3608371f49a62f7623cdcefb3c6f7b1f4132
oali1200 <83743492+oali1200@users.noreply.github.com>
2021-05-06 06:46:46 +0100

    Fixed bugs from last commit in the passing of arrays into functions

--- Top_level.cpp
+++ Top_level.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <utility> // std::pair
@@ -8,9 +9,9 @@
 using namespace std;
 
 //Declaring a function that generates the Linear Programs
-void Generate_LP(int round, float pricing[24], ofstream lpfile);
+void Generate_LP(int round, float *pricing, ofstream &lpfile);
 //Declaring a function that saves the values from the output text file
-void Copy_Text(ifstream lpout, ofstream finaloutput);
+void Copy_Text(ifstream &lpout, ofstream &finaloutput);
 
 int main()
 {
@@ -22,29 +23,48 @@ int main()
     int EnergyDemand[50];
     int ReadyTimeRound;
     int FinalTimeRound;
+    int cnt;
+    string price;
+    float pricing[25];
+    float pricefloat;
+    string line;
     
     //We want to open our files here
     ofstream lpfile;
     ofstream finaloutput;
-    string line;
     
     //We want to read the excel file here for user task allocations
-    ifstream f1("1.txt");
-    ifstream f2("2.txt");
-    ifstream f3("3.txt");
-    ifstream f4("4.txt");
-    ifstream fp("TrainingData.txt");
-    ifstream lpout("lpout.txt");
+    std::ifstream f1("1.txt");
+    std::ifstream f2("2.txt");
+    std::ifstream f3("3.txt");
+    std::ifstream f4("4.txt");
+    std::ifstream fp("TrainingData.txt");
+    std::ifstream lpout("lpout.txt");
     lpfile.open("output.txt");
     finaloutput.open("finaloutput.txt");
 
+    //Gets the user task Data and places it into arrays
+    for (cnt = 0; cnt < 50; cnt++)
+    {
+        f1 >> ReadyTime[cnt];
+        f2 >> Deadline[cnt];
+        f3 >> MaximumEnergy[cnt];
+        f4 >> EnergyDemand[cnt];
+    }
     //Create a for loop that iterates for each of the 10,000 pricing curves
     for (size_t round = 0; round < 10000; round++)
     {
         //Get the pricing values for one iteration
         
+        for (cnt = 0; cnt < 25; cnt++)
+        {
+            getline(fp, price,',');
+            pricefloat = stof(price);
+            pricing[cnt] = pricefloat;
+        }
+
         //Call a function that Generates the Linear Program while feeding into it the iteration we are in so that the function can use the correct pricing guideline
-        Generate_LP(round, )
+        Generate_LP(round, pricing, lpfile);
         //Use a system call that calls on lpsolve to run the LP
         //Call a function that saves the values from the output text file made by LP solve into seperate text files
 
@@ -63,7 +83,7 @@ int main()
 }
 
 //Create a function that generates the Linear Programs
-void Generate_LP(int round, float pricing[24], ofstream lpfile){
+void Generate_LP(int round, float *pricing, ofstream &lpfile){
 
 }
 
