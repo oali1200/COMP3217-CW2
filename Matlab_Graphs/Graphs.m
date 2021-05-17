a7ea563fb8c739ef50d16f837de78bda0fbf41cd
oali1200 <83743492+oali1200@users.noreply.github.com>
2021-05-17 22:55:50 +0100

    Matlab Code
    Created a function to generate a graph for the costs of the electricity used

--- Matlab_Graphs/Graphs.m
+++ Matlab_Graphs/Graphs.m
@@ -1,6 +1,42 @@
 Pricing = readtable('finaloutput.txt');
 
-for c = 0:4999
+Pa = table2array(Pricing);
+
+f1 = figure;
+f2 = figure;
+f3 = figure;
+f4 = figure;
+f5 = figure;
+figure(f1);
+for c = 1:5000
     x = 0:23;
-    y = Pricing(c,2:25);
+    y1 = Pa(c,2:25);
+    plot(x,y1)
+    hold on;
 end
+hold off
+figure(f2);
+for c = 5001:10000
+    x = 0:23;
+    y2 = Pa(c,2:25);
+    plot(x,y2)
+    hold on;
+end
+hold off;
+figure(f3);
+y = Pa(1:5000,1);
+histogram(y);
+hold on;
+hold off;
+figure(f4);
+y = Pa(5001:10000,1);
+histogram(y);
+hold on;
+hold off;
+figure(f5);
+x = 0:23;
+y = 0:22;
+z = Pa(1:5000,2:25);
+hist3(z);
+hold on;
+hold off;
